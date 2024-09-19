e56e734924dcfda60ae2bf4776410faacf88cdae
Favored Online Skills Acquisition Network <69410084+Favoredonline@users.noreply.github.com>
2024-09-19 13:49:42 -0400

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -25,7 +25,9 @@ const About = () => {
         </div>
         <div className=" text-white flex flex-col justify-center leading-7 md:leading-10">
           <h2 className="text-3xl font-bold mb-5">Meet Our Founder</h2>
-          <p className="mb-4">My name is <a href="https://www.linkedin.com/in/opeyemi-adeniran/" target="_blank" rel="noopener noreferrer" className="text-[#D2AE2F] font-semibold">Opeyemi T. Adeniran</a>. I am an AI Research Engineer and the founder of three initiatives: TechTalents City, She Model Tech, and <a href="https://www.morgantechfest.com/" target="_blank" rel="noopener noreferrer" className="text-[#D2AE2F] font-semibold">Morgan TechFest.</a></p>
+         <p className="mb-4">
+  Hello, I'm <a href="https://www.linkedin.com/in/opeyemi-adeniran/" target="_blank" rel="noopener noreferrer" className="text-[#D2AE2F] font-semibold">Opeyemi T. Adeniran</a>, an AI Research Engineer and the founder of three innovative initiatives: <strong className="text-[#D2AE2F]">TechTalents City</strong>, <strong className="text-[#EB48D5]">She Model Tech</strong>, and <a href="https://www.morgantechfest.com/" target="_blank" rel="noopener noreferrer" className="text-[#D2AE2F] font-semibold">Morgan TechFest</a>.
+</p>
         </div>
       </div>
     </div>
