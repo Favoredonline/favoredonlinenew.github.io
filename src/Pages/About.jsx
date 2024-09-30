5cf3a61abfb0108b46d4a85e0775a838ad0c16f9
Favored Online Skills Acquisition Network <69410084+Favoredonline@users.noreply.github.com>
2024-09-30 07:17:58 -0400

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -5,11 +5,7 @@ const About = () => {
       <div className="text-center">
         <p className="text-gray-100 font-bold text-3xl lg:text-4xl xl:text-5xl">About Favored Online</p>
    <p className="text-gray-100 col-span-4 mt-8">
-  <strong>Favored Online Inc.</strong>, founded in July 2018 and originally registered as Favored Online Skills Acquisition Network in March 2020.
-</p>
-<p className="text-gray-100 col-span-4 mt-4">
-  It serves as the parent holding company for two initiatives:<strong className="text-[#D2AE2F]">TechTalents City</strong> and <strong className="text-[#EB48D5]">She Model Tech.</strong>
-</p>
+  <strong>Favored Online Inc.</strong>, founded in July 2018 and originally registered as Favored Online Skills Acquisition Network in March 2020, serves as the parent holding company for two initiatives: <strong className="text-[#D2AE2F]">TechTalents City</strong> and <strong className="text-[#EB48D5]">She Model Tech</strong>, with a commitment to growth and innovation, and plans to introduce more initiatives in the future.
       </div>
       <div className="grid md:grid-cols-2 gap-8 md:gap-5 lg:gap-10 text-white my-12 lg:my-24 lg:mx-20 leading-7">
         <div>
